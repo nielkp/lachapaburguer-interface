48b7136e19d226e7e46522f78bde0823dded694a
Daniel Knaip <danielknaip@gmail.com>
2025-08-03 14:47:17 -0300

    Corrigido Notificações do Toastify!

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { Login } from './containers/Login';
 import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Login />
       <GlobalStyles />
+      <ToastContainer autoClose={2000} />
     </BrowserRouter>
   </StrictMode>
 );
