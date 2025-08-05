0536f00e8287e191b1d6d1dca8f505707fa54c4b
Daniel Knaip <danielknaip@gmail.com>
2025-08-04 23:11:30 -0300

    Configurado as Rotas!

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
+import { RouterProvider } from 'react-router-dom';
 import GlobalStyles from './styles/globalStyles';
-import { createRoot } from 'react-dom/client';
-import { Login } from './containers/Login';
-import { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import { router } from './routes';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Login />
-      <GlobalStyles />
-      <ToastContainer autoClose={2000} />
-    </BrowserRouter>
+    <RouterProvider router={router} />
+    <GlobalStyles />
+    <ToastContainer autoClose={2000} />
   </StrictMode>
 );
